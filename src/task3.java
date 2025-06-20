d937e92773fbc4d71fed330c951cbb163d8262c0
Azerty <nik.k.2018@mail.ru>
2025-06-20 21:59:11 +0300

    Задача 3: проверка скорости

---
+++ src/task3.java
@@ -0,0 +1,11 @@
+public class task3 {
+    public static void main(String[] args) {
+        int speed = 65;
+
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
+        }
+    }
+}
