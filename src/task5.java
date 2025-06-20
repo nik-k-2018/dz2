099851dce50064154c62eb804b3806eb3afc4b92
Azerty <nik.k.2018@mail.ru>
2025-06-20 22:18:34 +0300

    Задача 5: ограничение по возрасту на атракционы

---
+++ src/task5.java
@@ -0,0 +1,20 @@
+public class task5 {
+    public static void main(String[] args) {
+        int age = 10;
+        boolean hasAdult = true;
+
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+        } else if (age >= 5 && age < 14) {
+            if (hasAdult) {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+            } else {
+                System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе без взрослого");
+            }
+        } else if (age >= 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого");
+        } else {
+            System.out.println("Неизвестный возраст: " + age);
+        }
+    }
+}
