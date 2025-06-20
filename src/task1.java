21c56d37d23622a53c7a01e3b50183cfdcb76e39
Azerty <nik.k.2018@mail.ru>
2025-06-20 21:19:25 +0300

    Решение задачи 1: проверка возраста

---
+++ src/task1.java
@@ -0,0 +1,11 @@
+public class task1 {
+    public static void main(String[] args) {
+        int age = 17; // Можно изменить значение для проверки
+
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+}
