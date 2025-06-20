132ac000dc046fe30aa4d10094f08b97c58ad9f5
Azerty <nik.k.2018@mail.ru>
2025-06-20 21:46:33 +0300

    Задача 2: погода и одежда

---
+++ src/task2.java
@@ -0,0 +1,11 @@
+public class task2 {
+    public static void main(String[] args) {
+        int temperature = 3;
+
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+    }
+}
